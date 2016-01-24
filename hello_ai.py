11237db179f1123d99476aa9e12b895bfa191b09
peeranont <tom_pee49@hotmail.com>
2016-01-25 00:23:31 +0700

    hello_ai

---
+++ hello_ai.py
@@ -0,0 +1,6 @@
+Python 3.3.2 (v3.3.2:d047928ae3f6, May 16 2013, 00:03:43) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> n = '5622791663 Peeranont Jamornmarn'
+>>> print (n)
+5622791663 Peeranont Jamornmarn
+>>> 
